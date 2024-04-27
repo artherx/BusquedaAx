23fcdc621aeade24490af028dd144280983222a0
artherx <xxartherxx@gmail.com>
2024-04-27 00:10:50 -0500

    cambios minimos

--- src/components/nodo.js
+++ src/components/nodo.js
@@ -48,3 +48,4 @@ class Nodo {
         this.H = H
     }
 }
+export default Nodo
--- src/components/reglaJuego.js
+++ src/components/reglaJuego.js
@@ -1,9 +1,30 @@
-const matriz =[]
+/* import Nodo from "./nodo"; */
+const matriz = [];
 for (let i = 0; i < 8; i++) {
-    matriz[i] = [];
-    for (let j = 0; j < 8; j++) {
-      matriz[i][j] = 0;
+  matriz[i] = [];
+  for (let j = 0; j < 8; j++) {
+    matriz[i][j] = 0;
+  }
+}
+/* function heuristica(inicio, final) {
+  let d1 = Math.abs(final.x - inicio.x);
+  let d2 = Math.abs(final.y - inicio.y);
+  return d1 + d2;
+}
+function vencindario(x,y, matriz) {
+  let veci = [];
+
+  for (let i = 0; i < 3; i++) {
+    veci[i] = [];
+    for (let j = 0; j < 3; j++) {
+      veci[i][j] = new Nodo();
     }
   }
-
-export default {matriz}
+  veci[1][1]
+  if(y+1<8){
+    if(matriz[x][y+1]==0){
+      veci[1][y]
+    }
+  }
+} */
+export default { matriz };
